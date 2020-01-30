f3e8d67183bd1963f2fbd9be682b508865ddafe8
Chris Speck <cgspeck@gmail.com>
2020-01-30 21:47:51 +1100

    Configure TZ and locale

--- arch/bootstrap.sh
+++ arch/bootstrap.sh
@@ -1,5 +1,16 @@
 #! /bin/bash -e
 
+timedatectl set-ntp true
+
+# set up timezone
+ln -sf /usr/share/zoneinfo/Australia/Melbourne /etc/localtime
+# hwclock --systohc  # Not on a Raspberry Pi!
+
+# set up locales
+sed -i 's/#en_AU.UTF-8 UTF-8/en_AU.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+echo "LANG=en_AU.UTF-8" > /etc/locale.conf
+
 pacman -Sy
 pacman -S --needed - < pkglist.txt
 pacman -Su
