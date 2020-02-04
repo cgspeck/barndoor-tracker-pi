d6eb0bf8d5eacf066d2e3095a8c7e1989b423ae7
Chris Speck <cgspeck@gmail.com>
2020-02-04 20:39:16 +1100

    Less code is best code

--- backend/internal/handlers/debug.go
+++ backend/internal/handlers/debug.go
@@ -1,18 +1,14 @@
 package handlers
 
 import (
-	"encoding/json"
-	"io"
 	"net/http"
 )
 
 func DebugHandler(ah IAppHandler, w http.ResponseWriter, r *http.Request) (int, error) {
-	w.Header().Set("Content-Type", "application/json")
-	b, err := json.MarshalIndent(ah.GetContext(), "", "  ")
+	err := writeJson(ah.GetContext(), w)
 	if err != nil {
 		return 500, err
 	}
-	io.WriteString(w, string(b))
 
 	return 200, nil
 }
--- backend/internal/handlers/flags.go
+++ backend/internal/handlers/flags.go
@@ -5,12 +5,6 @@ import (
 )
 
 func Flags(ah IAppHandler, w http.ResponseWriter, r *http.Request) (int, error) {
-	if r.Method == "OPTIONS" {
-		w.Header().Set("Content-Type", "application/json")
-		return 204, nil
-	}
-
-	w.Header().Set("Content-Type", "application/json")
 	err := writeJson(ah.GetFlags(), w)
 	if err != nil {
 		return 500, err
