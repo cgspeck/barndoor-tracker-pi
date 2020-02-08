f8e0f2ddbf971c7c94ed604df6db744b4b452265
Chris Speck <cgspeck@gmail.com>
2020-02-08 15:12:55 +1100

    Update arch linux arm bootstrap to set up i2c

--- README.md
+++ README.md
@@ -8,6 +8,36 @@ Follow directions [here](https://archlinuxarm.org/platforms/armv8/broadcom/raspb
 
 Then run `./arch/bootstrap.sh` as root.
 
+Reboot, then with the LSM9DS1 connected to SPI Bus 1 as per the table below, run this command to verify communications: `sudo i2cdetect -y 1`
+
+Expected output:
+
+```
+     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
+00:          -- -- -- -- -- -- -- -- -- -- -- -- --
+10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- 1e --
+20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+40: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+60: -- -- -- -- -- -- -- -- -- -- -- 6b -- -- -- --
+70: -- -- -- -- -- -- -- --
+```
+
+Note:
+
+- `0x1e` is the default address of the magnetometer
+- `0x6b` is the default address of the accelerometer and gyroscope
+
+## LSM9DS1 Wiring
+
+| Rpi                  | LSM9DS1 |
+| -------------------- | ------- |
+| 3v3                  | VCC     |
+| Ground               | GND     |
+| 2<br/>BCM2<br/>WiPi8 | SDA     |
+| 3<br/>BCM3<br/>WiPi9 | SCL     |
+
 ## Updating snapshots
 
 ```
--- arch/bootstrap.sh
+++ arch/bootstrap.sh
@@ -15,6 +15,30 @@ pacman -Sy --noconfirm
 pacman -S --noconfirm --needed - < pkglist.txt
 pacman -Su
 
+# backup and install required system config
+set +e
+diff /boot/config.txt boot/config.txt > /dev/null
+rc=$?
+set -e
+if [[ 0 != $rc ]]; then
+    # cp /boot/config.txt /boot/config.txt.bak
+    cp boot/config.txt /boot/config.txt
+    echo -e "\n\nBoot config has been changed! \nPlease reboot to apply changes.\n\n"
+fi
+
+if [ ! -f /etc/systemd/resolved.conf.bak ]; then
+    cp /etc/systemd/resolved.conf /etc/systemd/resolved.conf.bak
+fi
+cp etc/systemd/resolved.conf /etc/systemd/resolved.conf
+
+systemctl restart systemd-resolved
+
+if [ ! -f /etc/modules-load.d/raspberrypi.conf.bak ]; then
+    cp /etc/modules-load.d/raspberrypi.conf /etc/modules-load.d/raspberrypi.conf.bak
+fi
+cp etc/modules-load.d/raspberrypi.conf /etc/modules-load.d/raspberrypi.conf
+
+# files that will be overwritten by the barndoor tracker itself
 if [ ! -f /etc/hostapd/hostapd.conf.bak ]; then
     cp /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bak
 fi
@@ -22,11 +46,3 @@ fi
 if [ ! -f /etc/dnsmasq.conf.bak ]; then
     cp /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
 fi
-
-if [ ! -f /etc/systemd/resolved.conf.bak ]; then
-    cp /etc/systemd/resolved.conf /etc/systemd/resolved.conf.bak
-fi
-
-cp etc/systemd/resolved.conf /etc/systemd/resolved.conf
-
-systemctl restart systemd-resolved
