326eeee620048525237d744a7fe0fb7dc4645dd5
Chris Speck <cgspeck@gmail.com>
2020-02-08 22:56:00 +1100

    start i2c POC and successfully get whoami checks to passgit statusgit status

---
+++ pocs/i2c/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+
+	"./lsm9ds1"
+
+	// "github.com/davecgh/go-spew/spew"
+	"github.com/kidoman/embd"
+	_ "github.com/kidoman/embd/host/rpi" // This loads the RPi driver
+)
+
+func main() {
+	log.Println("hello world")
+	bus := embd.NewI2CBus(1)
+	bus.Close()
+
+	mAddress := 0x1e
+	agAddress := 0x6b
+	mTest := make([]byte, 1)
+	agTest := make([]byte, 1)
+
+	err := bus.ReadFromReg(byte(mAddress), lsm9ds1.WHO_AM_I_M, mTest)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = bus.ReadFromReg(byte(agAddress), lsm9ds1.WHO_AM_I_XG, agTest)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if mTest[0] != lsm9ds1.WHO_AM_I_M_RSP {
+		log.Fatalln("Magnetometer whoam failed!")
+	}
+
+	if agTest[0] != lsm9ds1.WHO_AM_I_AG_RSP {
+		log.Fatalln("Accel/Gyro whoam failed!")
+	}
+	log.Println("whoam check pass")
+}
