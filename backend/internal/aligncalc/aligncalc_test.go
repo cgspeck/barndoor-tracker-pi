495b66bf607298e7d68838b867cfe2413fd63129
Chris Speck <cgspeck@gmail.com>
2020-04-22 22:36:04 +1000

    working on tests

--- backend/internal/aligncalc/aligncalc_test.go
+++ backend/internal/aligncalc/aligncalc_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 func TestAlignCalcIgnoreModes(t *testing.T) {
+	nilReading := []float32{0, 0, 0}
 
 	type TestCase struct {
 		ignoreAz           bool
@@ -43,7 +44,7 @@ func TestAlignCalcIgnoreModes(t *testing.T) {
 			IgnoreAz:  tt.ignoreAz,
 		}
 
-		CalculateAlignment(&align, &locationSettings, []float32{0, 0, 0})
+		CalculateAlignment(&align, &locationSettings, nilReading, nilReading)
 
 		e := tt.expectedAltAligned
 		a := align.AltAligned
@@ -60,9 +61,10 @@ func TestAlignCalcIgnoreModes(t *testing.T) {
 }
 
 func TestAlignCalcAltitude(t *testing.T) {
+	nilReading := []float32{0, 0, 0}
+
 	type TestCase struct {
 		ignoreAlt          bool
-		altErrorSetting    float64
 		latitudeSetting    float64
 		calcLatitude       float64
 		expectedAltAligned bool
@@ -133,12 +135,65 @@ func TestAlignCalcAltitude(t *testing.T) {
 			Latitude:  tt.latitudeSetting,
 		}
 
-		CalculateAlignment(&align, &locationSettings, tt.accelVal)
+		CalculateAlignment(&align, &locationSettings, tt.accelVal, nilReading)
 
 		e := tt.expectedAltAligned
 		a := align.AltAligned
 		if a != e {
 			t.Errorf("unexpected status: got: %v, want: %v", a, e)
+		}
+	}
+}
+
+func TestAlignCalcAzimuth(t *testing.T) {
+	nilReading := []float32{0, 0, 0}
+
+	type TestCase struct {
+		azErrorSetting      float64
+		declinationSetting  float64
+		expectedAzAligned   bool
+		expectedCalcHeading float64
+		ignoreAz            bool
+		latitudeSetting     float64
+		magVal              []float32
+	}
+
+	testCases := []TestCase{
+		TestCase{
+			ignoreAz:          true,
+			expectedAzAligned: true,
+		},
+		TestCase{
+			azErrorSetting:      0.1,
+			declinationSetting:  0.0,
+			expectedAzAligned:   true,
+			expectedCalcHeading: 10,
+			ignoreAz:            false,
+			latitudeSetting:     -37,
+			magVal:              []float32{},
+		},
+	}
+
+	for _, tt := range testCases {
+		align := models.AlignStatus{}
+		locationSettings := models.LocationSettings{
+			AzError:  tt.azErrorSetting,
+			IgnoreAz: tt.ignoreAz,
+			Latitude: tt.latitudeSetting,
+		}
+
+		CalculateAlignment(&align, &locationSettings, nilReading, tt.magVal)
+
+		eStatus := tt.expectedAzAligned
+		aStatus := align.AzAligned
+		if aStatus != eStatus {
+			t.Errorf("unexpected status: got: %v, want: %v", aStatus, eStatus)
+		}
+
+		eVal := tt.expectedCalcHeading
+		aVal := align.CurrentAz
+		if aVal != eVal {
+			t.Errorf("unexpected calculation: got: %v, want: %v", aVal, eVal)
 		}
 	}
 }
