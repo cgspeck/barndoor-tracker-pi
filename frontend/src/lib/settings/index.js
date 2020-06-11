c656dc96332762b4e82a0bbc8c335da02cf07df3
Chris Speck <cgspeck@gmail.com>
2020-06-11 22:46:50 +1000

    Finally got VS Code, Prettier and ESlint working together. Sort of.

--- frontend/src/lib/settings/index.js
+++ frontend/src/lib/settings/index.js
@@ -1,42 +1,46 @@
-import axios from "axios";
-import config from "../../config";
+import axios from 'axios';
+import config from '../../config';
 
 async function getAllSettings() {
-  return axios.get(`${config.endpoint}/status/debug`).then(r => r.data);
+  return axios.get(`${config.endpoint}/status/debug`).then((r) => r.data);
 }
 
 async function getAPSettings() {
-  return axios.get(`${config.endpoint}/settings/network/ap`).then(r => r.data);
+  return axios
+    .get(`${config.endpoint}/settings/network/ap`)
+    .then((r) => r.data);
 }
 
 async function setAPSettings(ssid, key) {
   // remove when frontend actually has wireless management features
   await axios.post(`${config.endpoint}/settings/network`, {
-    accessPointMode: true
+    accessPointMode: true,
   });
   return axios
     .post(`${config.endpoint}/settings/network/ap`, {
-      ssid: ssid,
-      key: key
+      ssid,
+      key,
     })
-    .then(r => r.data);
+    .then((r) => r.data);
 }
 
 async function getIntervalometerSettings() {
-  return axios.get(`${config.endpoint}/settings/intervalometer`).then(r => r.data);
+  return axios
+    .get(`${config.endpoint}/settings/intervalometer`)
+    .then((r) => r.data);
 }
 
 async function setIntervalometerSettings(bulbInterval, restInterval) {
   return axios
     .post(`${config.endpoint}/settings/intervalometer`, {
       bulbInterval,
-      restInterval
+      restInterval,
     })
-    .then(r => r.data);
+    .then((r) => r.data);
 }
 
 async function getLocationSettings() {
-  return axios.get(`${config.endpoint}/settings/location`).then(r => r.data);
+  return axios.get(`${config.endpoint}/settings/location`).then((r) => r.data);
 }
 
 async function setLocationSettings(
@@ -46,7 +50,7 @@ async function setLocationSettings(
   altError,
   xOffset,
   yOffset,
-  zOffset
+  zOffset,
 ) {
   return axios
     .post(
@@ -58,55 +62,56 @@ async function setLocationSettings(
         altError,
         xOffset,
         yOffset,
-        zOffset
+        zOffset,
       },
       {
         transformRequest: [
-          function(data, _) {
+          function (data, _) {
             const res = {
               latitude: parseFloat(data.latitude),
               magDeclination: parseFloat(data.magDeclination),
               azError: parseFloat(data.azError),
               altError: parseFloat(data.altError),
-              xOffset: parseInt(data.xOffset),
-              yOffset: parseInt(data.yOffset),
-              zOffset: parseInt(data.zOffset)
+              xOffset: parseInt(data.xOffset, 10),
+              yOffset: parseInt(data.yOffset, 10),
+              zOffset: parseInt(data.zOffset, 10),
             };
             return JSON.stringify(res);
-          }
-        ]
-      }
+          },
+        ],
+      },
     )
-    .then(r => r.data);
+    .then((r) => r.data);
 }
 
 async function getFlags() {
-  return axios.get(`${config.endpoint}/status/flags`).then(r => r.data);
+  return axios.get(`${config.endpoint}/status/flags`).then((r) => r.data);
 }
 
 async function getAlignStatus() {
-  return axios.get(`${config.endpoint}/status/align`).then(r => r.data);
+  return axios.get(`${config.endpoint}/status/align`).then((r) => r.data);
 }
 
 async function getInitialTrackStatus() {
-  return axios.get(`${config.endpoint}/track/all`).then(r => r.data);
+  return axios.get(`${config.endpoint}/track/all`).then((r) => r.data);
 }
 
 async function getTrackState() {
-  return axios.get(`${config.endpoint}/track`).then(r => r.data);
+  return axios.get(`${config.endpoint}/track`).then((r) => r.data);
 }
 
 async function getDewControllerStatus() {
-  return axios.get(`${config.endpoint}/status/dew_controller`).then(r => r.data);
+  return axios
+    .get(`${config.endpoint}/status/dew_controller`)
+    .then((r) => r.data);
 }
 
 async function setTargetTemperature(targetTemp) {
-  return axios.post(
-    `${config.endpoint}/settings/dew_controller`,
-    {
-      targetTemp
-    }
-    ).then(r => r.data);
+  return axios
+    .post(`${config.endpoint}/settings/dew_controller`, {
+      targetTemp,
+    })
+    .then((r) => r.data);
 }
 
 export {
@@ -122,5 +127,5 @@ export {
   getIntervalometerSettings,
   setIntervalometerSettings,
   getDewControllerStatus,
-  setTargetTemperature
+  setTargetTemperature,
 };
--- frontend/src/routes/track/index.js
+++ frontend/src/routes/track/index.js
@@ -1,16 +1,22 @@
-import { h, Component } from "preact";
-import style from "./style";
+import { h, Component } from 'preact';
+import style from './style';
 
-import Button from "preact-material-components/Button";
-import "preact-material-components/Button/style.css";
-import Switch from "preact-material-components/Switch";
-import "preact-material-components/Switch/style.css";
+import Button from 'preact-material-components/Button';
+import 'preact-material-components/Button/style.css';
+import Switch from 'preact-material-components/Switch';
+import 'preact-material-components/Switch/style.css';
 import TextField from 'preact-material-components/TextField';
-import "preact-material-components/TextField/style.css";
+import 'preact-material-components/TextField/style.css';
 
 import { getInitialTrackStatus, getTrackState } from '../../lib/settings';
-import { startHoming, startTracking, stopTracking, toggleIntervalometer, toggleDewController } from '../../lib/commands';
-import { setInterval } from "timers";
+import {
+  startHoming,
+  startTracking,
+  stopTracking,
+  toggleIntervalometer,
+  toggleDewController,
+} from '../../lib/commands';
+import { setInterval } from 'timers';
 
 export default class Track extends Component {
   state = {
@@ -19,43 +25,43 @@ export default class Track extends Component {
     dewControllerEnabled: null,
     intervalometerState: null,
     elapsedMillis: null,
-    error: null
+    error: null,
   };
 
   async componentDidMount() {
-		getInitialTrackStatus()
-			.then(r => {
+    getInitialTrackStatus()
+      .then((r) => {
         this.setState({
           trackingState: r.state,
           intervalometerEnabled: r.intervalometerEnabled,
           dewControllerEnabled: r.dewControllerEnabled,
-          elapsedMillis: r.elapsedMillis
-        })
-        console.log("Starting Refresh Interval");
+          elapsedMillis: r.elapsedMillis,
+        });
+        console.log('Starting Refresh Interval');
         this.timer = setInterval(this.refreshAlignmentStatus.bind(this), 1000);
       })
-			.catch(e => this.handleError(e));
-	}
-
-  handleError = e => {
-		console.error('problem', e)
-		this.setState({error: e});
+      .catch((e) => this.handleError(e));
   }
+
+  handleError = (e) => {
+    console.error('problem', e);
+    this.setState({ error: e });
+  };
 
   async refreshAlignmentStatus() {
     getTrackState()
-      .then(r => {
+      .then((r) => {
         this.setState({
           trackingState: r.state,
           intervalometerState: r.intervalometerState,
-          elapsedMillis: r.elapsedMillis
-        })
+          elapsedMillis: r.elapsedMillis,
+        });
       })
-      .catch(e => this.handleError(e));
+      .catch((e) => this.handleError(e));
   }
 
   componentWillUnmount() {
-    console.log("Cancelling timer");
+    console.log('Cancelling timer');
     clearInterval(this.timer._id);
   }
 
@@ -64,147 +70,166 @@ export default class Track extends Component {
       minutes = Math.floor((duration / (1000 * 60)) % 60),
       hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
-    hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
 
-    return hours + ":" + minutes + ":" + seconds;
+    return hours + ':' + minutes + ':' + seconds;
   }
 
   errorToast() {
-		if (this.state.error != null) {
-			return(
-				<p>
-					{ this.state.error.toString() }
-				</p>
-			)
-		}
+    if (this.state.error != null) {
+      return <p>{this.state.error.toString()}</p>;
+    }
   }
 
-  onHomePressed = e => {
+  onHomePressed = (e) => {
     e.preventDefault();
     startHoming()
-      .then(r => this.setState({ trackingState: r }))
-      .catch(e => this.handleError(e));
-  }
+      .then((r) => this.setState({ trackingState: r }))
+      .catch((e) => this.handleError(e));
+  };
 
-  onTrackPressed = e => {
+  onTrackPressed = (e) => {
     e.preventDefault();
     startTracking()
-      .then(r => this.setState({
-        trackingState: r,
-        intervalometerState: null,
-        elapsedMillis: 0,
-      }))
-      .catch(e => this.handleError(e));
-  }
+      .then((r) =>
+        this.setState({
+          trackingState: r,
+          intervalometerState: null,
+          elapsedMillis: 0,
+        }),
+      )
+      .catch((e) => this.handleError(e));
+  };
 
-  onStopPressed = e => {
+  onStopPressed = (e) => {
     e.preventDefault();
     stopTracking()
-      .then(r => this.setState({ trackingState: r }))
-      .catch(e => this.handleError(e));
-  }
+      .then((r) => this.setState({ trackingState: r }))
+      .catch((e) => this.handleError(e));
+  };
 
-  onIntervalometerToggled = e => {
+  onIntervalometerToggled = (e) => {
     const enabled = e.target.checked;
-    console.log(`Intervalometer toggled to: ${enabled ? "enabled" : "disabled"}`);
-    this.setState({intervalometerEnabled: enabled})
+    console.log(
+      `Intervalometer toggled to: ${enabled ? 'enabled' : 'disabled'}`,
+    );
+    this.setState({ intervalometerEnabled: enabled });
     toggleIntervalometer(enabled)
-      .then(r => this.setState({intervalometerEnabled: r}))
-      .catch(e => this.handleError(e));
-  }
+      .then((r) => this.setState({ intervalometerEnabled: r }))
+      .catch((e) => this.handleError(e));
+  };
 
-  onDewControllerEnabled = e => {
+  onDewControllerEnabled = (e) => {
     const enabled = e.target.checked;
-    console.log(`Dew controller toggled to: ${enabled ? "enabled" : "disabled"}`);
-    this.setState({dewControllerEnabled: enabled})
+    console.log(
+      `Dew controller toggled to: ${enabled ? 'enabled' : 'disabled'}`,
+    );
+    this.setState({ dewControllerEnabled: enabled });
     toggleDewController(enabled)
-      .then(r => this.setState({dewControllerEnabled: r}))
-      .catch(e => this.handleError(e));
-  }
+      .then((r) => this.setState({ dewControllerEnabled: r }))
+      .catch((e) => this.handleError(e));
+  };
 
   homeButton() {
     if (this.state.trackingState == 'Idle') {
-      return(
+      return (
         <p>
           <Button raised ripple onClick={this.onHomePressed.bind(this)}>
             Home
           </Button>
         </p>
-      )
+      );
     }
   }
 
   trackButton() {
     if (this.state.trackingState == 'Homed') {
-      return(
+      return (
         <p>
           <Button raised ripple onClick={this.onTrackPressed.bind(this)}>
             Track
           </Button>
         </p>
-      )
+      );
     }
   }
 
   stopButton() {
     if (this.state.trackingState == 'Tracking') {
-      return(
+      return (
         <p>
           <Button raised ripple onClick={this.onStopPressed.bind(this)}>
             Stop
           </Button>
         </p>
-      )
+      );
     }
   }
 
   intervalometerState() {
-    if (this.state.intervalometerEnabled === true && this.state.trackingState === 'Tracking' && this.state.intervalometerState != null) {
-      return(
+    if (
+      this.state.intervalometerEnabled === true &&
+      this.state.trackingState === 'Tracking' &&
+      this.state.intervalometerState != null
+    ) {
+      return (
         <p>
           <TextField
             label="Intervalometer"
             value={this.state.intervalometerState}
-            disabled="true"/>
+            disabled="true"
+          />
         </p>
-      )
+      );
     }
   }
 
   elapsedTime() {
-    if (this.state.trackingState === 'Tracking' && this.state.elapsedMillis != null) {
-      return(
+    if (
+      this.state.trackingState === 'Tracking' &&
+      this.state.elapsedMillis != null
+    ) {
+      return (
         <p>
           <TextField
             label="Elapsed Time"
             value={this.msToTime(this.state.elapsedMillis)}
-            disabled="true"/>
+            disabled="true"
+          />
         </p>
-      )
+      );
     }
   }
 
   render({}, { trackingState, intervalometerEnabled }) {
-    return(
+    return (
       <div class={style.track}>
         <h1>Track</h1>
         <div>
           {this.errorToast()}
           <p>
-            <TextField label="Tracking Status" value={trackingState} disabled="true"/>
+            <TextField
+              label="Tracking Status"
+              value={trackingState}
+              disabled="true"
+            />
           </p>
-          { this.elapsedTime() }
-          { this.intervalometerState() }
+          {this.elapsedTime()}
+          {this.intervalometerState()}
           <p>
-            Intervalometer: <Switch onChange={this.onIntervalometerToggled.bind(this)} checked={intervalometerEnabled === true}></Switch>
+            Intervalometer:{' '}
+            <Switch
+              onChange={this.onIntervalometerToggled.bind(this)}
+              checked={intervalometerEnabled === true}
+            ></Switch>
           </p>
-          { this.homeButton() }
-          { this.trackButton() }
-          { this.stopButton() }
+          {this.homeButton()}
+          {this.trackButton()}
+          {this.stopButton()}
         </div>
       </div>
-    )
+    );
   }
 }
