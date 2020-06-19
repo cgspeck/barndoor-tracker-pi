244e429a966f2cd165fbcffbcc7611754b1799dd
Chris Speck <cgspeck@gmail.com>
2020-06-19 19:47:47 +1000

    wip transforming new app into a log viewer

--- pid-log-viewer/src/App.js
+++ pid-log-viewer/src/App.js
@@ -1,27 +1,128 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import "fontsource-roboto";
+
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import LuxonUtils from "@date-io/luxon";
+
+import Button from "@material-ui/core/Button";
+import { DateTimePicker } from "@material-ui/pickers";
+
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  ReferenceLine,
+  ReferenceArea,
+  ReferenceDot,
+  Tooltip,
+  CartesianGrid,
+  Legend,
+  Brush,
+  ErrorBar,
+  AreaChart,
+  Area,
+  Label,
+  LabelList,
+} from "recharts";
+import { scalePow, scaleLog } from "d3-scale";
+
 import "./App.css";
+
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+const scale = scaleLog().base(10).nice();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          yx
+      <MuiPickersUtilsProvider utils={LuxonUtils}>
+        <div className="App">
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <DateTimePicker label="Start" />
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <p>
+            <DateTimePicker label="End" />
+          </p>
+          <p>
+            <Button variant="contained">Zoom</Button>
+          </p>
+          <p>
+            <Button variant="contained" color="secondary">
+              Reset
+            </Button>
+          </p>
+          <p>A simple LineChart with customized line dot</p>
+          <LineChart
+            width={1000}
+            height={600}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="pv"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+        </div>
+      </MuiPickersUtilsProvider>
     );
   }
 }
